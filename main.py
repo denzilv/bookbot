ec69e4b7ea7e8326fad8b59013515c9c05fc174c
Denzil Villarico <dev@denzilv.com>
2024-09-23 13:39:01 -0700

    Update Bookbot to Dockerize

--- main.py
+++ main.py
@@ -1,34 +1,54 @@
-
 def main():
-    with open('books/frankenstein.txt') as f:
-        file_contents = f.read()
-        words = file_contents.split()
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
 
-    dict = {}
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
 
-    for word in words:
-        for letter in word.lower():
-            if letter not in dict:
-                dict[letter] = 1
-            else:
-                dict[letter] += 1
-    
-    dictlist = []
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
 
-    for key, value in dict.items():
-        if key.isalpha():
-            temp = {"letter": key, "count": value}
-            dictlist.append(temp)
-
-    def sort_on(dict):
-        return dict["count"]
-
-    dictlist.sort(reverse=True, key=sort_on)
-
-    print("--- Begin report of books/frankenstein.txt ---")
-    print(len(words), "words found in the document\n")
-    for each in dictlist:
-        print(f'The \'{each['letter']}\' character was found {each['count']} times')
     print("--- End report ---")
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
 
 main()
